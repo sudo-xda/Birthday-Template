92ed9d3d7f773153fac57966dc5e6a65eda7795c
Jigyansha Jeevan Rout <62542350+sudo-xda@users.noreply.github.com>
2021-05-23 08:51:03 +0530

    Update

--- js/index.js
+++ js/index.js
@@ -294,7 +294,7 @@ function reveal() {
   }
 
   var ifrm = document.createElement("iframe");
-  ifrm.setAttribute("src", "https://www.youtube.com/embed/_QpCwBPEF70?controls=0&loop=1&autoplay=1");
+  ifrm.setAttribute("src", "pic.jpg");
   //ifrm.style.width = `${w}px`;
   //ifrm.style.height = `${h}px`;
   ifrm.style.border = 'none';
